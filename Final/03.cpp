53be28ec712762a6d7fb7544cf4f75fc525333aa
Ahanaf Tahmid <45955172+AhanafTahmid@users.noreply.github.com>
2023-11-23 22:19:47 +0600

    .

--- Final/03.cpp
+++ Final/03.cpp
@@ -6,7 +6,7 @@ int A(int m, int n){
     if(m==0 && n!=0){
         return n+1;
     }
-    if(m!=0 && n==0) return A(m-1,1);
+    else if(m!=0 && n==0) return A(m-1,1);
     else if(m!=0 && n!=0) return A(m-1,A(m,n-1));
 }
 
